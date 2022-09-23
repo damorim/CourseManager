ce0849fcf41f09537986400d6e5533ec1eb52d33
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2022-09-23 14:42:00 -0300

    modifying Angular's root component to show elements of the Student's form

--- gui/src/app/app.component.ts
+++ gui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gui';
+   student: Student = {name: "", id: "", email: ""};
+}
+
+export class Student {
+  name: string = "";
+  id: string = "";
+  email: string = "";
 }
