60fb7ddc8c7914570a543c27512837817e78dc2d
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2022-09-24 12:29:51 -0300

    refactoring save method

--- gui/src/app/student.service.ts
+++ gui/src/app/student.service.ts
@@ -2,12 +2,17 @@ import { Student } from './student';
 
 export class StudentService {
   students: Student[] = [];
+  
   save(student: Student): Student {
      var result = null as any;
-     if (!this.students.find(s => s.id == student.id)) {
+     if (this.idAlreadyRegistered(student.id)) {
        this.students.push(student);     
        result = student;
      }
      return result;     
+  }
+
+  idAlreadyRegistered(id: string): boolean {
+     return !this.students.find(s => s.id == id);
   }
 }
